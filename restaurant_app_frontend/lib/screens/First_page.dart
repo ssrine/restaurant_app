b5fff4570a35b32f3d4d7263b2e482632655df32
ssrine <harkaninissrine81@gmail.com>
2024-11-13 11:17:59 +0100

    Add some modifications in template (color + animations) First_page

--- restaurant_app_frontend/lib/screens/First_page.dart
+++ restaurant_app_frontend/lib/screens/First_page.dart
@@ -16,7 +16,7 @@ class _OnbordingState extends State<Onbording> with SingleTickerProviderStateMix
     super.initState();
     _controller = PageController(initialPage: 0);
     _animationController = AnimationController(
-      vsync: this, // Now works with the mixin
+      vsync: this,
       duration: Duration(milliseconds: 500),
     );
   }
@@ -31,140 +31,171 @@ class _OnbordingState extends State<Onbording> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFDEDEC),
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView.builder(
-              controller: _controller,
-              itemCount: contents.length,
-              onPageChanged: (int index) {
-                setState(() {
-                  currentIndex = index;
-                  _animationController.forward(from: 0); // Restart animation
-                });
-              },
-              itemBuilder: (_, i) {
-                return Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      // Container with a fade-in scale animation on the image
-                      ScaleTransition(
-                        scale: Tween<double>(begin: 0.9, end: 1.0).animate(
-                          CurvedAnimation(
-                            parent: _animationController,
-                            curve: Curves.easeInOut,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFFD5E53), // Lighter red (for the top of the screen)
+              Color(0xFFC0392B), // Mid red color
+              Color(0xFF8E2C1A), // Darker red (for the bottom of the screen)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _controller,
+                itemCount: contents.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                    _animationController.forward(from: 0);
+                  });
+                },
+                itemBuilder: (_, i) {
+                  return Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ScaleTransition(
+                          scale: Tween<double>(begin: 0.9, end: 1.0).animate(
+                            CurvedAnimation(
+                              parent: _animationController,
+                              curve: Curves.easeInOut,
+                            ),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
+                                ),
+                              ],
+                            ),
+                            child: Image.asset(
+                              contents[i].image,
+                              height: MediaQuery.of(context).size.height * 0.3,
+                              fit: BoxFit.contain,
+                            ),
                           ),
                         ),
-                        child: Container(
-                          color: Color(0xFFFDEDEC),
-                          padding: const EdgeInsets.all(20),
-                          child: Image.asset(
-                            contents[i].image,
-                            height: MediaQuery.of(context).size.height * 0.3,
-                            fit: BoxFit.contain,
+                        SizedBox(height: 30),
+                        FadeTransition(
+                          opacity: _animationController,
+                          child: Text(
+                            contents[i].title,
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  blurRadius: 5.0,
+                                  color: Colors.black.withOpacity(0.6),
+                                  offset: Offset(2.0, 2.0),
+                                ),
+                              ],
+                            ),
+                            textAlign: TextAlign.center,
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      // Animated title text
-                      FadeTransition(
-                        opacity: _animationController,
-                        child: Text(
-                          contents[i].title,
-                          style: TextStyle(
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF4E0D0D),
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      // Animated description
-                      Expanded(
-                        child: SingleChildScrollView(
-                          child: FadeTransition(
-                            opacity: _animationController,
-                            child: Text(
-                              contents[i].discription,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Color(0xFF555555),
+                        SizedBox(height: 20),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: FadeTransition(
+                              opacity: _animationController,
+                              child: Text(
+                                contents[i].description,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white70,
+                                  height: 1.5, // Adds line height for better readability
+                                  letterSpacing: 0.5, // Adds spacing between letters
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-
-          // Dots Indicator with smooth width transition
-          Container(
-            margin: EdgeInsets.only(bottom: 20),
-            child: Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: List.generate(
                 contents.length,
                 (index) => AnimatedContainer(
                   duration: Duration(milliseconds: 300),
-                  margin: EdgeInsets.only(right: 5),
+                  margin: EdgeInsets.symmetric(horizontal: 4),
                   height: 10,
                   width: currentIndex == index ? 25 : 10,
                   decoration: BoxDecoration(
-                    color: Color(0xFF8B0000),
+                    color: Colors.white,
                     borderRadius: BorderRadius.circular(20),
+                    boxShadow: currentIndex == index
+                        ? [BoxShadow(color: Colors.white, blurRadius: 4)]
+                        : [],
                   ),
                 ),
               ),
             ),
-          ),
-
-          // Next/Continue Button with animation
-          Padding(
-            padding: EdgeInsets.all(40),
-            child: SizedBox(
-              width: double.infinity,
-              height: 60,
-              child: ElevatedButton(
-                child: Text(
-                  currentIndex == contents.length - 1 ? "Continue" : "Next",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                ),
-                onPressed: () {
-                  if (currentIndex == contents.length - 1) {
-                    // Navigate to the home page when the last page is reached
-                    // Navigator.pushReplacement(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (_) => Home(), // Replace with actual Home widget
-                    //   ),
-                    // );
-                  } else {
-                    _controller.nextPage(
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.easeInOut,
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF8B0000),
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
+                width: double.infinity,
+                height: 55,
+                child: ElevatedButton(
+                  child: Text(
+                    currentIndex == contents.length - 1 ? "Continue" : "Next",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.2, // Adds spacing for a sleek effect
+                    ),
+                  ),
+                  onPressed: () {
+                    if (currentIndex == contents.length - 1) {
+                      // Navigate to the home page
+                      // Navigator.pushReplacement(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (_) => Home(),
+                      //   ),
+                      // );
+                    } else {
+                      _controller.nextPage(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Color(0xFF8E2C1A), // Updated darker red
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    shadowColor: Colors.black38,
+                    elevation: 5,
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- restaurant_app_frontend/lib/screens/content_model.dart
+++ restaurant_app_frontend/lib/screens/content_model.dart
@@ -1,13 +1,13 @@
 class UnbordingContent {
   final String image;
   final String title;
-  final String discription;
+  final String description; // Changed from 'discription' to 'description'
 
   // Making the fields required
   UnbordingContent({
     required this.image,
     required this.title,
-    required this.discription,
+    required this.description, // Changed here as well
   });
 }
 
@@ -16,19 +16,19 @@ List<UnbordingContent> contents = [
   UnbordingContent(
     title: 'Gourmet Quality',
     image: 'assets/images/loogo.jpg',
-    discription: "Experience the best in dining with restaurant-quality food. From appetizers to desserts, "
+    description: "Experience the best in dining with restaurant-quality food. From appetizers to desserts, "
                  "every dish is crafted to deliver exceptional taste and quality right to you. Enjoy fine dining at your convenience.",
   ),
   UnbordingContent(
     title: 'Nearby Spots',
     image: 'assets/images/mapresto2.png',
-    discription: "Use our interactive map to find nearby dining options. Discover popular restaurants around you, "
+    description: "Use our interactive map to find nearby dining options. Discover popular restaurants around you, "
                  "explore new spots, and enjoy seamless navigation to satisfy your cravings wherever you are.",
   ),
   UnbordingContent(
     title: 'Smart Chatbot',
     image: 'assets/images/aichatboot.png',
-    discription: "Need a recommendation? Our AI chatbot is here to help! Chat for personalized restaurant suggestions "
+    description: "Need a recommendation? Our AI chatbot is here to help! Chat for personalized restaurant suggestions "
                  "and make reservations at top-rated spots, tailored to your tastes. Enjoy instant, intelligent assistance.",
   ),
 ];
